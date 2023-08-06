63773f9f7feee0918c48bef6d78137ca37395d8d
iiuliia <mekaeva.ylia@yandex.ru>
2023-08-06 23:01:40 +0500

    Задание 2

---
+++ task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+// стоящих на нечётных индексах.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int [] array = GetArray(5, 1, 10);
+Console.WriteLine($"[{String.Join(",", array)}]");
+int sum = 0;
+for(int i = 0; i < array.Length; i++){
+    sum += i % 2 != 0 ? array[i] : 0;
+}
+Console.WriteLine(sum);
+int[] GetArray(int size, int minValue, int maxValue){
+    int[] result = new int[size];
+    for(int i = 0; i < size; i++){
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
